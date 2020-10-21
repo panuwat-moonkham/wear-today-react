0c05f9fc67776fde2b697f264f84e1b0db7ae03f
devillkaow <devillkaow@gmail.com>
2020-10-21 19:56:27 +0700

    update postPage

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,9 +1,41 @@
 import React from "react";
+import styled from "styled-components"
+import { Title, Subtitle } from '../components/Typography'
+
+const Navbarcontainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #000;
+`
+
+
+
+const Content = styled.div`
+  padding: 10rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #ccc;
+  
+`
 
 function Post() {
   return(
       <>
-      <wrapper></wrapper>
+      <Navbarcontainer>
+      <Title color="">Post</Title>
+      </Navbarcontainer>
+      <Content id="content">
+        <input/>
+        <input/>
+        <input/>
+        <input/>
+        <input/>
+      </Content>
+      
       </>
   )
 }
