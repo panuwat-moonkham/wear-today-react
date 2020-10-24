39b9d67c2be13cd6905b007177567456fefae04b
devillkaow <devillkaow@gmail.com>
2020-10-25 01:25:42 +0700

    add Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,22 +3,44 @@ import styled from 'styled-components'
 import BackgroundBlock from '../components/BackgroundBlock'
 import image from '../assets/img2.jpg'
 
+const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-bottom: 4rem;
+  font-size: 80px;
+`
 const Container = styled.div`
+  padding: 4rem 10rem;
 `
 
 const Content = styled.div`
-  padding: 8rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;  
 `
 
-function Contact () {
+function Contact() {
   return (
     <>
-      <BackgroundBlock src={image} height='30' />
+      <BackgroundBlock src={image} height="30" />
       <Container>
-        <Content id='content' />
+        <Title>Contact</Title>
+        <Content>
+        
+            <div>Image</div>
+            <div>IG</div>
+         
+            <div>Image</div>
+            <div>Faceookb</div>
+        
+            <div>Image</div>
+            <div>IG</div>
+        
+        </Content>
       </Container>
     </>
   )
 }
 
-export default Contact;
+export default Contact
