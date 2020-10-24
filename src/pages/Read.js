aa0e72452526a457c00627475e8301cbf374ab20
panuwat-moonkham <panuwat.moonkham00@gmail.com>
2020-10-24 15:16:22 +0700

    add read page

---
+++ src/pages/Read.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import BackgroundBlock from '../components/BackgroundBlock'
+import image from '../assets/img2.jpg'
+
+const Container = styled.div`
+  padding: 2rem 6rem;
+`
+
+const Content = styled.div`
+  height:100vh;
+  width:100%;
+  background-color:#333;
+`
+
+function Read () {
+  return (
+    <>
+      <BackgroundBlock src={image} height='30' />
+      <Container>
+        <Content id='content' />
+      </Container>
+    </>
+  )
+}
+
+export default Read
