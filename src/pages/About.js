d6cb4f960881425a99c441e8eecebab28418e672
devillkaow <devillkaow@gmail.com>
2020-10-24 23:44:43 +0700

    edit About

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,19 +3,30 @@ import styled from 'styled-components'
 import BackgroundBlock from '../components/BackgroundBlock'
 import image from '../assets/img2.jpg'
 
+const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-bottom: 4rem;
+  font-size: 80px;
+`
+
 const Container = styled.div`
+  padding: 4rem 10rem;
 `
 
-const Content = styled.div`
-  padding: 8rem;
-`
+const Content = styled.div``
 
-function About () {
+function About() {
   return (
     <>
-      <BackgroundBlock src={image} height='30' />
+      <BackgroundBlock src={image} height="30" />
       <Container>
-        <Content id='content' />
+      <Title>About</Title>
+        <Content id="content">
+          <h1>ABOUT WearToday</h1>
+          <p>WearToday เป็นเว็บบล็อคสำหรับแลกเปลี่ยนสไตล์ เทคนิคและความเหมาะสมในการแต่งตัว</p>
+          <p></p>
+        </Content>
       </Container>
     </>
   )
