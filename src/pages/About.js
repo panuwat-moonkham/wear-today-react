221dec3e476f2a4f268426ee857d8a9a2e4abae7
devillkaow <devillkaow@gmail.com>
2020-10-24 16:02:38 +0700

    edit about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+
+const arr = new Array(250).fill('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean condimentum maximus eros,')
+
+const Title = styled.h1`
+  padding-top: 8rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Content = styled.div`
+  padding: 8rem;
+`
 
 function About() {
-  return <div>About page</div>
+  return (<>
+    <Title>About us</Title>
+    <Content id="content">
+        {arr.map((text, index) => (
+          <p key={index}>{text}</p>
+        ))}
+      </Content>
+  </>
+  )
 }
 
 export default About
