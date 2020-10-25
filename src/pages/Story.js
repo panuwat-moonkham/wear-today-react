08742fe05ecd2bc21f6579935e43bd6696824766
panuwat-moonkham <panuwat.moonkham00@gmail.com>
2020-10-25 22:01:19 +0700

    edit story page

--- src/pages/Story.js
+++ src/pages/Story.js
@@ -1,20 +1,21 @@
-//global fetch
+/* eslint-disable no-undef */
+// global fetch
 
-import React, { useState } from 'react'
+import React from 'react'
 import { useParams } from 'react-router-dom'
 import withHelmet from '../utils/withHelmet'
 
-async function getPost(id) {
-  const response = await fetch.get(`http://localhost/api/v1/posts/${id}`)
+async function getPost (id) {
+  const response = await fetch.get(`http://localhost:3333/api/v1/users/${id}`)
   return response.json()
 }
 
-function usePostDate({ postId }) {
+function useUserDate ({ Id }) {
   const [post, setPost] = useState({})
   const [isLoading, setLoading] = useState(false)
   const [error, setError] = useState(undefined)
 
-  async function reteiveData() {
+  async function reteiveData () {
     try {
       setLoading(true)
       const postResponse = await getPost(postId)
@@ -28,14 +29,16 @@ function usePostDate({ postId }) {
   return [{ post, isLoading, error }, { reteiveData }]
 }
 
-function Story() {
-  const { storyId } = useParams()
-  const [{ story, isLoading, error }, { reteiveData }] = usePostDate(storyId)
+function Story () {
+  const { Id } = useParams()
+  const [{ story, isLoading, error }, { reteiveData }] = useUserDate(Id)
 
   useEffect(() => {
     reteiveData()
   }, [reteiveData])
-  return <div>Story page {storyId}</div>
+  return (
+    <div>Story page {storyId}{story}{isLoading}{error}</div>
+  )
 }
 
 export default withHelmet('Blog App | Story')(Story)
