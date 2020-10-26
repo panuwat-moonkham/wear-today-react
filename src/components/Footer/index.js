2652db2fe2ee1dbd45925c583eac728a5d8f9403
devillkaow <devillkaow@gmail.com>
2020-10-27 06:11:12 +0700

    editColor Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,27 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
+import BackgroundBlock from '../BackgroundBlock'
+import logoImage from '../../assets/logo.png'
 
 const Container = styled.div`
-  height: 300px;
+  height: 180px;
   width: 100%;
   z-index: 10000;
-  background-color: black;
+  background-color: #eb8f4c;
   color: ${props => (props.isScroll ? '#000' : '#fff')};
   transition: background-color 0.5s;
 `
 const Wrapper = styled.div`
+  height:90%;
   display: flex;
-  flex: 1;
-  padding: 2rem;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 16px;
 `
 
-const Logo = styled.div``
-
-function Footer () {
+function Footer() {
   return (
     <Container>
       <Wrapper>
-        <Logo />
+        
+          <BackgroundBlock src={logoImage} height="15" width="8" />
+          WearToday © 2020 what do you wanna wear today
+        
       </Wrapper>
     </Container>
   )
