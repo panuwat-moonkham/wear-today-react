6e4b70e2f5ac2586cc8e68288e49b92fa28d8978
devillkaow <devillkaow@gmail.com>
2020-10-27 01:04:00 +0700

    move Home

---
+++ src/components/HomeContent/index.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import {Title, Container, Item, More, Content} from './styles'
+import { Link } from 'react-router-dom'
+import ShowItem from '../ShowItem'
+import image from '../../assets/img2.jpg'
+import logoImage from '../../assets/logo.png'
+import SlideShow from '../SlideShow'
+
+function HomeContent() {
+  return (
+    <>
+      <Container>
+        <SlideShow />
+        <Content>
+          <Title>TREND</Title>
+          <Item>
+            <ShowItem src={image}>
+              <h4>Nature</h4>
+              <p>What a beautiful sunrise</p>
+            </ShowItem>
+            <ShowItem src={image}>
+              <h4>Nature</h4>
+              <p>What a beautiful sunrise</p>
+            </ShowItem>
+            <ShowItem src={image}>
+              <h4>Nature</h4>
+              <p>What a beautiful sunrise</p>
+            </ShowItem>
+          </Item>
+
+          <More>
+            <Link>more...</Link>
+          </More>
+        </Content>
+        <Content>
+          <Title>THE JOURNEY</Title>
+          <Item>
+            <ShowItem src={image}>
+              <h4>Nature</h4>
+              <p>What a beautiful sunrise</p>
+            </ShowItem>
+
+            <ShowItem src={image}>
+              <h4>Nature</h4>
+              <p>What a beautiful sunrise</p>
+            </ShowItem>
+          </Item>
+          <More>
+            <Link>more...</Link>
+          </More>
+        </Content>
+        <Content>
+          <Title>STYLE</Title>
+          <Item>
+            <ShowItem src={image}>
+              <h4>Nature</h4>
+              <p>What a beautiful sunrise</p>
+            </ShowItem>
+            <ShowItem src={image}>
+              <h4>Nature</h4>
+              <p>What a beautiful sunrise</p>
+            </ShowItem>
+            <ShowItem src={image}>
+              <h4>Nature</h4>
+              <p>What a beautiful sunrise</p>
+            </ShowItem>
+          </Item>
+          <More>
+            <Link>more...</Link>
+          </More>
+        </Content>
+        <Item>
+          <ShowItem src={logoImage} height="40" overlay="none" />
+          {/* <BackgroundBlock src={logoImage} height='40' width='17' /> */}
+          <ShowItem src={image} height="40" overlay="none" />
+          <ShowItem src={image} height="40" overlay="none" />
+          <ShowItem src={image} height="40" overlay="none" />
+          <ShowItem src={image} height="40" overlay="none" />
+          <ShowItem src={image} height="40" overlay="none" />
+        </Item>
+      </Container>
+    </>
+  )
+}
+export default HomeContent
---
+++ src/components/HomeContent/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Title = styled.div`
+  text-align: center;
+  font-size: 4rem;
+  font-weight: bold;
+  padding: 1rem 0;
+  margin: 0 25%;
+  border-bottom: 1px solid;
+`
+
+export const Container = styled.div``
+
+export const Content = styled.div``
+
+export const More = styled.p`
+  font-size: 1.5rem;
+  text-align: right;
+`
+
+export const Item = styled.div`
+  display: flex;
+`
