9499d9d511cba0768d7c0f6252aab9707020c088
panuwat-moonkham <panuwat.moonkham00@gmail.com>
2020-10-27 07:20:24 +0700

    add Read in folder

--- src/pages/Read.js
+++ src/pages/Read.js
@@ -1,29 +1,10 @@
 import React from 'react'
-import styled from 'styled-components'
-import BackgroundBlock from '../components/BackgroundBlock'
 import ReadContent from '../components/Read'
-import image from '../assets/img2.jpg'
-
-const Container = styled.div`
-  padding: 2rem 6rem;
-`
-
-const Content = styled.div`
-  height: 100vh;
-  width: 100%;
-  background-color: #333;
-`
 
 function Read () {
   return (
     <>
-      <BackgroundBlock src={image} height='30' overlay='overlay' />
-      <Container>
-        <Content id='content'>
-          <ReadContent />
-
-        </Content>
-      </Container>
+      <ReadContent />
     </>
   )
 }
