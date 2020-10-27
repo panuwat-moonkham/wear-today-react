4b44b49501cd4ad556c70b70b92d244dd8436972
panuwat-moonkham <panuwat.moonkham00@gmail.com>
2020-10-27 07:20:00 +0700

    add ReadPost page

---
+++ src/pages/ReadPost.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReadPostContent from '../components/ReadPost'
+
+function ReadPost () {
+  return (
+    <>
+      <ReadPostContent />
+    </>
+  )
+}
+
+export default ReadPost
